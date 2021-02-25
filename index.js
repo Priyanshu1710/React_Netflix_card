8bb8a4be7293e995eee176211af1465fde2cf881
Priyanshu Gupta <62554204+Priyanshu1710@users.noreply.github.com>
2021-02-25 13:38:03 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Card from './Card_netflix_01'
+import "./netflix.css";
+
+ReactDOM.render(
+    <>
+        <Card 
+        imgsrc='https://wallpapercave.com/uwp/uwp374924.jpeg'
+        title='A Netflix Original Series '
+        sname="DARK"
+        link='https://www.netflix.com/in/title/80100172'
+        />
+        <Card 
+        imgsrc='https://wallpapercave.com/wp/wp7418539.jpg'
+        title='A Netflix Original Series '
+        sname='Extra Curricular'
+        link='https://www.netflix.com/in/title/80990668'
+        />
+        <Card 
+        imgsrc='https://wallpapercave.com/wp/wp4431942.jpg'
+        title='A Netflix Original Series '
+        sname='Stranger Things'
+        link='https://www.netflix.com/in/title/80057281'
+        />
+        <Card 
+        imgsrc='https://wallpapercave.com/uwp/uwp374924.jpeg'
+        title='A Netflix Original Series '
+        sname="DARK"
+        link='https://www.netflix.com/in/title/80100172'
+        />
+        <Card 
+        imgsrc='https://wallpapercave.com/wp/wp7418539.jpg'
+        title='A Netflix Original Series '
+        sname='Extra Curricular'
+        link='https://www.netflix.com/in/title/80990668'
+        />
+        <Card 
+        imgsrc='https://wallpapercave.com/wp/wp4431942.jpg'
+        title='A Netflix Original Series '
+        sname='Stranger Things'
+        link='https://www.netflix.com/in/title/80057281'
+        />
+
+    </>
+    , document.getElementById('root')
+);
